ffe2421d3fd6369463be309cea92a2f35c5435f4
inescapableDelusion <79125763+inescapableDelusion@users.noreply.github.com>
2021-02-16 19:17:36 +0800

    Update test.py

--- test.py
+++ test.py
@@ -15,3 +15,5 @@ def fibonacci(n):
 #this is a test in order to stop the [sample] issue
 print("The factorial of 6 is {}.".format(factorial(6))
 print("The 15th fibonacci term is {}.".format(fibonacci(15))
+      
+#this is just for the sample lol
