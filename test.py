7f88e5b0a6ef4d26ae5251d5e78cae5310190799
Farrel Beso <homonescius@gmail.com>
2021-02-16 20:14:24 +0800

    Test test testicles

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ print("Hello World")
 #this is to be repeated again and again and again.
 def factorial(n):
     if n == 0 or n == 1: return 1
-    else: return factorial(n-1)
+    else: return n * factorial(n-1)
     
 #fibonacci version of recursion
 def fibonacci(n):
@@ -14,8 +14,8 @@ def fibonacci(n):
 
 #let us test our recursion functions
 #this is a test in order to stop the [sample] issue
-print("The factorial of 6 is {}.".format(factorial(6))
-print("The 15th fibonacci term is {}.".format(fibonacci(15))
+print("The factorial of 6 is {}.".format(factorial(6)))
+print("The 15th fibonacci term is {}.".format(fibonacci(15)))
       
 #this is just for the sample lol
 #anytime now? lol
