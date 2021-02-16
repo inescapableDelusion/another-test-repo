14702971afae985abc98073fbdc50a3875786eea
inescapableDelusion <79125763+inescapableDelusion@users.noreply.github.com>
2021-02-16 19:11:28 +0800

    Issue is resolved, hopefully

--- test.py
+++ test.py
@@ -10,3 +10,8 @@ def fibonacci(n):
     if n == 1: return 0
     elif n == 2: return 1
     else: return fibonacci(n-1) + fibonacci(n-2)
+
+#let us test our recursion functions
+#this is a test in order to stop the [sample] issue
+print("The factorial of 6 is {}.".format(factorial(6))
+print("The 15th fibonacci term is {}.".format(fibonacci(15))
